927a0322909800b90bf61843026c49c3a687731f
Mustaque Khan <130723242+mustaque789@users.noreply.github.com>
2024-05-28 17:06:04 -0400

    cm1

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import Loader from "./Components/Loader";
+import CryptoList from "./Components/CryptoList";
+
+const Home = () => {
+  const [card, setCard] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+
+  const getCardData = async () => {
+    const res = await fetch(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=12&page=${page}&sparkline=false`
+    );
+    const data = await res.json();
+    // console.log(data);
+    setCard((prev) => [...prev, ...data]);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getCardData();
+  }, [page]);
+
+  const handelInfiniteScroll = async () => {
+    // console.log("scrollHeight" + document.documentElement.scrollHeight);
+    // console.log("innerHeight" + window.innerHeight);
+    // console.log("scrollTop" + document.documentElement.scrollTop);
+    try {
+      if (
+        window.innerHeight + document.documentElement.scrollTop + 1 >=
+        document.documentElement.scrollHeight
+      ) {
+        setLoading(true);
+        setPage((prev) => prev + 1);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handelInfiniteScroll);
+    return () => window.removeEventListener("scroll", handelInfiniteScroll);
+  }, []);
+
+  return (
+    <>
+      <CryptoList coinsData={card} />
+      {loading && <Loader />}
+    </>
+  );
+};
+
+export default Home;
